43b8073f3ed9a87624a0f592381c4da0c13e56f3
Mike <Mikemountain.dev@gmail.com>
2020-05-27 14:14:59 +0200

    working on the layout and basic styles of the app

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -2,11 +2,19 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {LayoutComponent} from './components/layout/layout.component';
 import {RouterModule} from '@angular/router';
+import { PrimaryNavigationComponent } from './components/primary-navigation/primary-navigation.component';
+import { SideNavigationComponent } from './components/side-navigation/side-navigation.component';
+import { ContentNavigationComponent } from './components/content-navigation/content-navigation.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    LayoutComponent
+    LayoutComponent,
+    PrimaryNavigationComponent,
+    SideNavigationComponent,
+    ContentNavigationComponent,
+    FooterComponent
   ],
   exports: [
     LayoutComponent
