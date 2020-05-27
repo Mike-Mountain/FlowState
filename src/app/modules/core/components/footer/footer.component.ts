74370525879aeb11e677d337430491cf755fee11
Mike <Mikemountain.dev@gmail.com>
2020-05-27 19:56:37 +0200

    Finalize the layout before beginning to add functionality and content

--- src/app/modules/core/components/footer/footer.component.ts
+++ src/app/modules/core/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, NgZone, OnInit, Renderer2, ViewChild} from '@angular/core';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('date') public dateControl: ElementRef;
+  public date: Date;
+
+  private datePipe = new DatePipe('en-za');
+
+  constructor(private zone: NgZone,
+              private renderer: Renderer2) {
+    zone.runOutsideAngular(() => {
+      setInterval(() => {
+        renderer.setProperty(this.dateControl.nativeElement, 'textContent', this.datePipe.transform(new Date(), 'HH:mm:ss'));
+      }, 1000);
+    });
+  }
 
   ngOnInit(): void {
+    this.date = new Date();
   }
 
 }
