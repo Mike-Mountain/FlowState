1fb41a7636928ccb3223b67d42cd75d918e3c1cb
Mike <Mikemountain.dev@gmail.com>
2021-02-11 12:42:49 +0200

    Update to Angular10

--- src/app/modules/shared/services/color-picker-service/color-picker.service.ts
+++ src/app/modules/shared/services/color-picker-service/color-picker.service.ts
@@ -1,5 +1,7 @@
 import {BehaviorSubject, Observable} from 'rxjs';
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class ColorPickerService {
 
   private colorPallet = new BehaviorSubject<{ color, colorValues }>({color: 'rgba(255, 255, 255, 1}', colorValues: '255, 255, 255'});
